7130456f2867d26e0966059be2cd3c89399c87c9
galiciaNova <holapp334@gmail.com>
2026-03-15 11:33:43 -0600

    tarjetas de asignacion en clientes

--- vistas/header.php
+++ vistas/header.php
@@ -12,6 +12,7 @@
     <link rel="stylesheet" href="../public/css/plantilla.css">
     <link rel="stylesheet" href="../public/datatable/dataTables.bootstrap4.min.css">
     <link rel="stylesheet" href="../public/datatable/responsive.bootstrap4.min.css">
+    <link rel="stylesheet" href="../public/fontawesome/css/all.css">
     <title>Help-Desk</title>
 </head>
 <body>
