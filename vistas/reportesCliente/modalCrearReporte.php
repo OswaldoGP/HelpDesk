8989f4cf1c87e12d02c0be955d6390c365222304
galiciaNova <holapp334@gmail.com>
2026-03-15 13:02:14 -0600

    maquetar tabla de reportes

--- vistas/reportesCliente/modalCrearReporte.php
+++ vistas/reportesCliente/modalCrearReporte.php
@@ -1,19 +1,26 @@
 
 <!-- Modal -->
-<div class="modal fade" id="modalCrearReporte" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
-                <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                ...
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save changes</button>
+<form id="frmNuevoReporte" method="POST" onsubmit="return agregarNuevoReporte()" >
+    <div class="modal fade" id="modalCrearReporte" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Nuevo Reporte</h1>
+                    <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <label for="idEquipo">Mis dispositivos</label>
+                    <select name="idEquipo" id="idEquipo" class="form-control" required>
+                        <option value="">Selecciona un dispositivo</option>
+                    </select>
+                    <label for="problema">Describe tu problema</label>
+                    <textarea name="problema" id="problema" class="form-control" required></textarea>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                    <button class="btn btn-primary">Crear</button>
+                </div>
             </div>
         </div>
     </div>
-</div>
+</form>
--- vistas/reportesCliente/tablaReporteCliente.php
+++ vistas/reportesCliente/tablaReporteCliente.php
@@ -1,11 +1,36 @@
 <table class="table table-sm dt-responsive nowrap" 
-        style="width:100%" id="tablaAsignacionDataTable">
+        style="width:100%" id="tablaAsignacionDataTable" id="tablaReportesClienteDataTable" >
     <thead>
-        <th></th>
+        <th>#</th>
+        <th>Apellido Paterno</th>
+        <th>Apellido materno</th>
+        <th>Apellido nombre</th>
+        <th>Dispositivo</th>
+        <th>Fecha</th>
+        <th>Descripcion</th>
+        <th>Estatus</th>
+        <th>Solucion</th>
+        <th>Eliminar</th>
+
     </thead>
     <tbody>
         <tr>
             <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
         </tr>
     </tbody>
 </table>
+
+<script>
+    $(document).ready(function(){
+        $('#tablaReportesClienteDataTable').DataTable();
+    });
+</script>
