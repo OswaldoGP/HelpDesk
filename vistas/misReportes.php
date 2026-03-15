66ecc43230bddc27f1f2de2ccc12badf43315a38
galiciaNova <holapp334@gmail.com>
2026-03-15 12:18:03 -0600

    maquetacion del modulo de reportes

--- vistas/misReportes.php
+++ vistas/misReportes.php
@@ -7,13 +7,24 @@
 <div class="container">
     <div class="card border-0 shadow my-5">
         <div class="card-body p-5">
-        <h1 class="fw-light">Reportes</h1>
-        <p class="lead">In this snippet</p>
+            <h1 class="fw-light">Reportes de cliente </h1>
+            <p class="lead">
+                <button class="btn btn-primary"  data-toggle="modal" data-target="#modalCrearReporte">
+                    Crear reporte
+                </button>
+                <hr>
+                <div id="tablaReporteClienteLoad"></div>
+            </p>
+        </div>
     </div>
 </div>
 
 <?php 
+    include "reportesCliente/modalCrearReporte.php";
     include "footer.php"; 
+?>
+    <script src="../public/js/reportesCliente/reportesCliente.js"></script>
+<?php
     } else {
         header("location:../index.html");
     }
---
+++ vistas/reportesCliente/tablaReporteCliente.php
@@ -0,0 +1,11 @@
+<table class="table table-sm dt-responsive nowrap" 
+        style="width:100%" id="tablaAsignacionDataTable">
+    <thead>
+        <th></th>
+    </thead>
+    <tbody>
+        <tr>
+            <td></td>
+        </tr>
+    </tbody>
+</table>
